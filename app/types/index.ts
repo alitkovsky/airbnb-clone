107a074330fdd0757524ff7980464e34467325c2
alitkovsky <alitkovsky@me.com>
2023-06-21 18:02:29 +0200

    Changed Listing type on SafeLitsting to delete warning.

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,11 @@
-import { User } from "next-auth";
+import { Listing, User } from "@prisma/client";
+
+export type SafeListing = Omit<
+   Listing,
+   "createdAt"
+> & {
+   createdAt: string;
+};
 
 export type SafeUser = Omit<
    User,

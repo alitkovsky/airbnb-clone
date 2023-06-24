5af1c4f874c0879fc51729c44e3e01fe2e03e638
alitkovsky <alitkovsky@me.com>
2023-06-24 20:40:33 +0200

    Finished loading trips with server component.

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -7,14 +7,13 @@ import { format } from "date-fns";
 
 import useCountries from "@/app/hooks/useCountries";
 
-import { SafeListing, SafeUser } from "@/app/types";
-import { Reservation } from "@prisma/client";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
    data: SafeListing;
-   reservation?: Reservation;
+   reservation?: SafeReservation;
    onAction?: (id: string) => void;
    disabled?: boolean;
    actionLabel?: string;

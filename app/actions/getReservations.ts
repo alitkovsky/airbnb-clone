cf94b594ef421fd68121c6d1544fc3c3a48396c2
alitkovsky <alitkovsky@me.com>
2023-06-25 19:03:48 +0200

    Finished Reservations screen. Login modal apears now after successful Sign up.

--- app/actions/getReservations.ts
+++ app/actions/getReservations.ts
@@ -1,56 +1,55 @@
 import prisma from "@/app/libs/prismadb";
 
-interface iParams {
-   listingId?: string;
-   userId?: string;
-   authorId?: string;
+interface IParams {
+  listingId?: string;
+  userId?: string;
+  authorId?: string;
 }
 
 export default async function getReservations(
-   params: iParams
+  params: IParams
 ) {
-   try {   
-      const { listingId, userId, authorId } = params;
+  try {
+    const { listingId, userId, authorId } = params;
 
-      const query: any = {};
+    const query: any = {};
+        
+    if (listingId) {
+      query.listingId = listingId;
+    };
 
-      if (listingId) {
-         query.listingId = listingId;
+    if (userId) {
+      query.userId = userId;
+    }
+
+    if (authorId) {
+      query.listing = { userId: authorId };
+    }
+
+    const reservations = await prisma.reservation.findMany({
+      where: query,
+      include: {
+        listing: true
+      },
+      orderBy: {
+        createdAt: 'desc'
       }
-      
-      if (userId) {
-         query.userId = userId;
-      }
+    });
 
-      if (authorId) {
-         query.lisitng = { userID: authorId };
-      }
+    const safeReservations = reservations.map(
+      (reservation) => ({
+      ...reservation,
+      createdAt: reservation.createdAt.toISOString(),
+      startDate: reservation.startDate.toISOString(),
+      endDate: reservation.endDate.toISOString(),
+      listing: {
+        ...reservation.listing,
+        createdAt: reservation.listing.createdAt.toISOString(),
+      },
+    }));
 
-      const reservations = await prisma.reservation.findMany({
-         where: query,
-         include: {
-            listing: true,
-         },
-         orderBy: {
-            createdAt: 'desc'
-         }
-      });
-
-      const safeReservations = reservations.map(
-         (reservation) => ({
-            ...reservation,
-            createdAt: reservation.createdAt.toISOString(),
-            startDate: reservation.startDate.toISOString(),
-            endDate: reservation.endDate.toISOString(),
-            listing: {
-               ...reservation.listing,
-               createdAt: reservation.listing.createdAt.toISOString()
-            }
-         })
-      );
-
-      return safeReservations;
-   } catch (error: any) {
-      throw new Error(error);
-   }
+    return safeReservations;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -107,7 +107,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                            label="My favorites"
                         />
                         <MenuItem 
-                           onClick={() => {}}
+                           onClick={() => {router.push("/reservations")}}
                            label="My reservations"
                         />
                         <MenuItem 
